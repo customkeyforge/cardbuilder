d783c04dc2ff566bb8d5a1128d3af5bc1af761a4
customkeyforge <customkeyforge@gmail.com>
2022-08-12 23:14:10 -0500

    Got PDF export working

--- load.js
+++ load.js
@@ -28,6 +28,7 @@ var controlAember = "\u001F";
 var controlDamage = "\u001C";
 import { downloadZip } from "https://cdn.jsdelivr.net/npm/client-zip/index.js";
 import { cardElement } from "./utils.js"
+const { jsPDF } = window.jspdf;
 
 class ColorManager {
     #parent = {};
@@ -671,32 +672,58 @@ async function setup() {
     };
 
     let cardWidth = 715;
+    let cardPrintWidth  = cardWidth;
     let cardHeight = 1000;
+    let cardPrintHeight  = cardHeight;
     let main = document.getElementById('global');
 
     let exportStyle = document.getElementById('exportStyle');
     let exportZip = document.getElementById('exportZip');
     let defaultCardBack = document.getElementById('globalCardBackImg');
     let exportCardBacks = document.getElementById('exportCardBacks');
+    let exportMessage = document.getElementById('exportMessage');
+    var progBase = document.getElementById("myProgress");
+    var progBar = document.getElementById("myBar");
+    let exportProgress = (percent, message) => {
+        console.log(message);
+        exportMessage.textContent = message;
+        progBar.style.width = percent + "%";
+    };
     exportZip.onclick = async () => {
-        globalMessage("Exporting zip file for printing.");
+        progBase.style.display = "block";
+        progBar.style.display = "block";
+        exportProgress(0, "Rebuilding JSON.");
         let jsonString = rebuildJSON();
         let rowCount = 3;
         let colCount = 3;
-        if (exportStyle.value == "2x2png") {
+        if (exportStyle.value == "2x2png" || exportStyle.value == "2x2pdf") {
             rowCount = 2;
             colCount = 2;
-        } else if (exportStyle.value == "1x1png") {
+        } else if (exportStyle.value == "1x1png" || exportStyle.value == "1x1pdf") {
             rowCount = 1;
             colCount = 1;
         }
+        let contextWidth = colCount * cardWidth;
+        let contextHeight = rowCount * cardHeight;
+        let doc = {};
+        let pdftop = 0;
+        let pdfleft = 0;
+        let pdfExport = exportStyle.value.endsWith("pdf"); 
+        if (pdfExport) {
+            cardPrintWidth = 63.5;
+            cardPrintHeight = 88;
+            //            doc = new jsPDF({unit: "mm", format: `[${colCount * cardPrintWidth},${rowCount * cardPrintHeight}]`});
+            doc = new jsPDF({unit: "mm", format: "letter"});
+            pdfleft = (doc.internal.pageSize.getWidth() / 2) - ((cardPrintWidth * colCount) / 2);
+            pdftop = (doc.internal.pageSize.getHeight() / 2) - ((cardPrintHeight * rowCount) / 2);
+        }
         let hugeCanvas = document.createElement("canvas");
-        hugeCanvas.width = cardWidth * colCount;
-        hugeCanvas.height = cardHeight * rowCount;
+        hugeCanvas.width = contextWidth;
+        hugeCanvas.height = contextHeight;
         let hugeContext = hugeCanvas.getContext('2d');
         let backCanvas = document.createElement("canvas");
-        backCanvas.width = cardWidth * colCount;
-        backCanvas.height = cardHeight * rowCount;
+        backCanvas.width = contextWidth;
+        backCanvas.height = contextHeight;
         let backContext = backCanvas.getContext('2d');
         let currentRow = 0;
         let currentCol = 0;
@@ -706,21 +733,35 @@ async function setup() {
         let fileName = "";
         let backFileName = "";
         let pageNumber = 1;
-        globalMessage("Started Creating card pages.");
+        let isFirstPage = true;
+        function cacheCanvas(canvas) {
+            let pageCanvas = document.createElement("canvas");
+            pageCanvas.width = contextWidth;
+            pageCanvas.height = contextHeight;
+            pageCanvas.className = "canvascache";
+            let pageContext = pageCanvas.getContext('2d');
+            pageContext.drawImage(canvas, 0,0, contextWidth, contextHeight);
+            return pageCanvas;
+        }
+        let progPerCard = 60 / cards.length;
+        exportProgress(10, "Creating Cards");
         for (var index in cards) {
             let element = cards[index];
+            let ctitle = cardElement(element.id, 'cardTitle');
+            exportProgress(progPerCard * index, `Redrawing ${ctitle.value}`);
             draw(element.id);
             let quantity = cardElement(element.id, 'quantity');
             let customCardBack = cardElement(element.id, 'customCardBackImg');
-            let ctitle = cardElement(element.id, 'cardTitle');
             let bigcan = cardElement(element.id, 'bigcanvas');
             for (var i = 0; i < quantity.value; i++) {
+                console.log(`Redrawing ${ctitle.value}`);
+
                 if (currentCol == 0 && currentRow == 0) {
-                    console.log("clearing canvas");
-                    hugeContext.clearRect(0,0,cardWidth * 3, cardHeight * 3);
-                    backContext.clearRect(0,0,cardWidth * 3, cardHeight * 3);
+                    hugeContext.fillStyle = "white";
+                    hugeContext.fillRect(0, 0, contextWidth, contextHeight);
+                    backContext.fillStyle = "white";
+                    backContext.fillRect(0, 0, contextWidth, contextHeight);
                 }
-                console.log(`drawing ${element.id} in position ${currentCol}, ${currentRow}`)
                 pageSaved = false;
                 fileName = `page${pageNumber}.png`;
                 backFileName = `page${pageNumber}(Back).png`;
@@ -729,22 +770,36 @@ async function setup() {
                     backFileName = `${ctitle.value} Copy ${i + 1}(Back).png`
                 }
                 hugeContext.drawImage(bigcan, cardWidth * currentCol, cardHeight * currentRow);
-                
                 if (exportCardBacks.checked) {
                     let backImg = defaultCardBack;
                     if (customCardBack.src != null && customCardBack.src.startsWith("data:image"))
                         backImg = customCardBack;
                     backContext.drawImage(backImg, cardWidth * (colCount - currentCol - 1) , cardHeight * currentRow);
-                }
+               }
                 if (currentCol == (colCount - 1) && currentRow == (rowCount - 1)){
                     currentCol = 0; currentRow = 0;
                     pageSaved = true;
-                    console.log("saving page to zip");
-                    let blob = await new Promise(resolve => hugeCanvas.toBlob(resolve));
-                    imagePages.push({ name: fileName, lastModified: new Date(), input: blob});
-                    if (exportCardBacks.checked) {
-                        let backblob = await new Promise(resolve => backCanvas.toBlob(resolve));
-                        imagePages.push({ name: backFileName, lastModified: new Date(), input: backblob});
+                    if (pdfExport) {
+                        let cache = cacheCanvas(hugeCanvas);
+                        if (isFirstPage == false) 
+                            doc.addPage();
+                        doc.addImage(cache, pdfleft, pdftop, cardPrintWidth * colCount, cardPrintHeight * rowCount,  null, "slow");
+
+                        if (exportCardBacks.checked) {
+                            doc.addPage();
+                            let cache = cacheCanvas(backCanvas);
+                            doc.addImage(cache, pdfleft, pdftop, cardPrintWidth * colCount, cardPrintHeight * rowCount,  null, "slow");
+                        }
+                        isFirstPage = false;
+                    }
+                    else {
+                        console.log("saving page to zip");
+                        let blob = await new Promise(resolve => hugeCanvas.toBlob(resolve));
+                        imagePages.push({ name: fileName, lastModified: new Date(), input: blob});
+                        if (exportCardBacks.checked) {
+                            let backblob = await new Promise(resolve => backCanvas.toBlob(resolve));
+                            imagePages.push({ name: backFileName, lastModified: new Date(), input: backblob});
+                        }
                     }
                     pageNumber++;
                 }
@@ -758,28 +813,54 @@ async function setup() {
         }
         if (pageSaved == false) {
             console.log("saving final not-full page.");
-            let blob = await new Promise(resolve => hugeCanvas.toBlob(resolve));
-            imagePages.push({ name: fileName, lastModified: new Date(), input: blob});
-            if (exportCardBacks.checked) {
-                let backblob = await new Promise(resolve => backCanvas.toBlob(resolve));
-                imagePages.push({ name: backFileName, lastModified: new Date(), input: backblob});
+            if (pdfExport) {
+                if (isFirstPage == false)
+                    doc.addPage();
+                let cache = cacheCanvas(hugeCanvas);
+                doc.addImage(cache, pdfleft, pdftop, cardPrintWidth * colCount, cardPrintHeight * rowCount,  null, "slow");
+                if (exportCardBacks.checked) {
+                    doc.addPage(); 
+                    let cache = cacheCanvas(backCanvas);
+                    doc.addImage(cache, pdfleft, pdftop, cardPrintWidth * colCount, cardPrintHeight * rowCount,  null, "slow");
+                }
+                doc.addPage();
+            }
+            else {
+                let blob = await new Promise(resolve => hugeCanvas.toBlob(resolve));
+                imagePages.push({ name: fileName, lastModified: new Date(), input: blob});
+                if (exportCardBacks.checked) {
+                    let backblob = await new Promise(resolve => backCanvas.toBlob(resolve));
+                    imagePages.push({ name: backFileName, lastModified: new Date(), input: backblob});
+                }
             }
         }
+        exportProgress(70, "Finished Creating card pages.");
+
         let deckname = document.getElementById('deckname').value;
         if (deckname == null || deckname == "")
             deckname = "adventure";
+        if (pdfExport) {
+            exportProgress(70, "Generating PDF.");
+            imagePages.push({ name: `${deckname}.pdf`, lastModified: new Date(), input: doc.output('blob')});
+        }
+
         imagePages.push({ name: `${deckname}.json`, lastModified: new Date(), input: jsonString})
-        globalMessage("Finished Creating card pages.");
+        exportProgress(80, "Building zip archive.");
         let zipblob = await downloadZip(imagePages).blob();
         //main.appendChild(hugeCanvas);
         var a = document.createElement("a");
         a.href = URL.createObjectURL(zipblob);
         a.download = `${deckname}.zip`;
-        globalMessage("Initiating zip download.");
+        exportProgress(95, "Initiating zip download.");
         a.click();
         a.remove();
+        document.querySelectorAll(".cachecanvas").forEach(cc => {
+            cc.remove();
+        });
         hugeCanvas.remove();
         backCanvas.remove();
+        exportProgress(100, "Export complete");
+
     };
 
     aemberImage = document.getElementById('aember');
